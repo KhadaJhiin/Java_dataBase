d9142b39f48511fb6fd32411b4a15df498219aba
KhadaJhiin <josemiguelortegagaviria@gmail.com>
2024-09-03 11:56:19 -0500

    Se termina la clase clienteDAO

--- src/main/java/clientes_mtd/data/ClienteDAO.java
+++ src/main/java/clientes_mtd/data/ClienteDAO.java
@@ -1,8 +1,6 @@
 package clientes_mtd.data;
-
 import clientes_mtd.connection.Conexion;
 import clientes_mtd.domain.Client;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -162,34 +160,104 @@ public class ClienteDAO implements IClienteDAO{
 
     @Override
     public boolean addClient(Client client) {
+
+        //We prepared statement for sentences sql
+
+        PreparedStatement ps;
+        Connection con = Conexion.getConnection();
+        String sql = "INSERT INTO cliente(nombre,apellido,ciudad,direccion,telefono,puntosPC)"
+                + "VALUES(?,?,?,?,?,?)";
+
+        try{
+            ps = con.prepareStatement(sql);
+            ps.setString(1, client.getNombre());
+            ps.setString(2, client.getApellido());
+            ps.setString(3, client.getCiudad());
+            ps.setString(4, client.getDireccion());
+            ps.setString(5, client.getTelefono());
+            ps.setInt(6, client.getPuntosPC());
+            ps.execute();
+            return true;
+
+        }catch (Exception e){
+            System.out.println("Error to add client: "+e.getMessage());
+        }
+        finally {
+            try {
+                con.close();
+            }catch (Exception e){
+                System.out.println("Error to close connection: "+e.getMessage());
+            }
+        }
+
         return false;
     }
 
     @Override
     public boolean modifyClient(Client client) {
+
+        //We prepared statement for sentences sql
+
+        PreparedStatement ps;
+        Connection con = Conexion.getConnection();
+        String sql = "UPDATE cliente SET nombre =?, apellido=?, ciudad=?, direccion=?, telefono=?, puntosPC=? "
+                +"WHERE idcliente = ?";
+
+        try {
+
+            ps = con.prepareStatement(sql);
+
+            ps.setString(1,client.getNombre());
+            ps.setString(2,client.getApellido());
+            ps.setString(3,client.getCiudad());
+            ps.setString(4,client.getDireccion());
+            ps.setString(5,client.getTelefono());
+            ps.setInt(6,client.getPuntosPC());
+            ps.setInt(7,client.getIdcliente());
+
+            ps.execute();
+            return true;
+
+        }catch (Exception e){
+            System.out.println("Error to update client: "+e.getMessage());
+        }finally {
+            try {
+                con.close();
+            }catch (Exception e){
+                System.out.println("Error to close connection: "+e.getMessage());
+            }
+        }
+
         return false;
     }
 
     @Override
     public boolean deleteClient(Client client) {
+
+        //We prepared statement for sentences sql
+
+        PreparedStatement ps;
+        Connection con = Conexion.getConnection();
+        String sql = "DELETE FROM cliente WHERE idcliente = ?";
+
+        try {
+
+            ps = con.prepareStatement(sql);
+            ps.setInt(1,client.getIdcliente());
+            ps.execute();
+            return true;
+
+        }catch (Exception e){
+            System.out.println("Error to delete client: "+e.getMessage());
+        }
+        finally {
+            try {
+                con.close();
+            }catch (Exception e){
+                System.out.println("Error to close connection:"+e.getMessage());
+            }
+        }
+
         return false;
-    }
-
-    public static void main(String[] args) {
-
-        IClienteDAO clientes = new ClienteDAO();
-
-//        var lista = clientes.listClients();
-//
-//        lista.forEach(System.out::println);
-
-        var cliente2 = new Client("Jose");
-
-        var encontrado = clientes.searchClientByName(cliente2);
-
-        System.out.println("Cliente encontrado: " + cliente2);
-
-
-
     }
 }
