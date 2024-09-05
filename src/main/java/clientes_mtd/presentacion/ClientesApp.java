7791e22ed7bcb1636c40654498dafd00b13e7742
KhadaJhiin <josemiguelortegagaviria@gmail.com>
2024-09-04 20:49:38 -0500

    Se termina el menu de la aplicacion

--- src/main/java/clientes_mtd/presentacion/ClientesApp.java
+++ src/main/java/clientes_mtd/presentacion/ClientesApp.java
@@ -69,6 +69,104 @@ public class ClientesApp {
                 runApp();
                 break;
 
+            case 3:
+                System.out.println("\nWrite name of the client to search");
+                String nombreCliente= input.nextLine();
+                Client clientOptionThre = new Client(nombreCliente);
+                client.searchClientByName(clientOptionThre);
+                System.out.println("\nDates of the client find \n"
+                        +clientOptionThre+"\n-----------------------------\n");
+                menu();
+                runApp();
+                break;
+
+            case 4:
+
+                System.out.println("\n--Insert dates of client--\n");
+
+                System.out.print("Name: ");
+                String name = input.nextLine();
+
+                System.out.print("\nLast name: ");
+                String lastName = input.nextLine();
+
+                System.out.print("\nCity: ");
+                String city = input.nextLine();
+
+                System.out.print("\nAddress: ");
+                String address = input.nextLine();
+
+                System.out.print("\nPhone");
+                String phone = input.nextLine();
+
+                System.out.print("\nPoints:");
+                int points = Integer.parseInt(input.nextLine());
+
+                Client clientOptionFour = new Client(name,lastName,city,address,phone,points);
+
+                client.addClient(clientOptionFour);
+
+                System.out.println("\nThe client was added successfully\n");
+
+                menu();
+                runApp();
+                break;
+
+            case 5:
+
+                System.out.println("Insert id of client to modify: ");
+                int idUser = Integer.parseInt(input.nextLine());
+                Client clExist = new Client(idUser);
+                boolean exist = client.searchClientById(clExist);
+
+                if(exist){
+                    System.out.println("\n--Insert dates of client to modify--\n");
+
+                    System.out.print("Name: ");
+                    String nameFive = input.nextLine();
+
+                    System.out.print("\nLast name: ");
+                    String lastNameFive = input.nextLine();
+
+                    System.out.print("\nCity: ");
+                    String cityFive = input.nextLine();
+
+                    System.out.print("\nAddress: ");
+                    String addressFive = input.nextLine();
+
+                    System.out.print("\nPhone");
+                    String phoneFive = input.nextLine();
+
+                    System.out.print("\nPoints:");
+                    int pointsFive = Integer.parseInt(input.nextLine());
+
+                    Client clientOptionFive = new Client(idUser,nameFive,lastNameFive,cityFive,addressFive,phoneFive,pointsFive);
+
+                    client.modifyClient(clientOptionFive);
+
+                    System.out.println("\nThe client was modify successfully\n");
+
+                    menu();
+                    runApp();
+                    break;
+
+                }else {
+                    System.out.println("Id client not found, try again");
+                    menu();
+                    runApp();
+                    break;
+                }
+
+            case 6:
+                System.out.println("Insert id of client to delete: ");
+                int idClientDelete = Integer.parseInt(input.nextLine());
+                Client clientDelete = new Client(idClientDelete);
+                client.deleteClient(clientDelete);
+                System.out.println("The client was delete successfully");
+                menu();
+                runApp();
+                break;
+
             case 7:
                 System.out.println("Thank you for used our application");
                 break;
